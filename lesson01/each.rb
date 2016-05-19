4da117a1b6fd660d76153d2b0c04a3b08af47309
Darren Burgess <darrentburgess@gmail.com>
2016-05-18 20:31:00 -0400

    initial commit with first exercises

---
+++ lesson01/each.rb
@@ -0,0 +1,29 @@
+[1, 2, 3].each do |num|
+  puts num
+end
+
+puts ''
+
+3.times do |num|
+  puts num
+end
+
+puts ''
+
+arr = [1, 2, 3].map do |num|
+  num + 1
+end
+
+puts arr.to_s
+
+puts ''
+
+File.open('tmp.txt', 'w') do |file|
+  file.write('first line!')
+end
+
+5.times do
+  |num|
+  puts num
+end
+
