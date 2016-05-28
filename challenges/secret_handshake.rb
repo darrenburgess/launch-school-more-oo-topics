fc5e1b3c5bc2a4fbba27a16d0b7aeede364438a3
Darren Burgess <darrentburgess@gmail.com>
2016-05-28 11:30:50 -0400

    complete challenge. all tests pass.

--- challenges/secret_handshake.rb
+++ challenges/secret_handshake.rb
@@ -1,4 +1,31 @@
 require 'pry'
 
 class SecretHandshake
+  attr_reader :number
+  attr_accessor :result
+
+  def initialize number
+    @number = number
+    @result = []
+  end
+
+  def to_binary
+    # http://www.wikihow.com/Convert-from-Decimal-to-Binary
+    number.to_s(2)
+  end
+
+  def is_true?(value)
+    value == '1'
+  end
+
+  def commands
+    return [] if number.class == String
+    array = self.to_binary.split('').reverse
+    result << "wink" if is_true? array[0]
+    result << "double blink" if is_true? array[1]
+    result << "close your eyes" if is_true? array[2]
+    result << "jump" if is_true? array[3]
+    result.reverse! if is_true? array[4]
+    result
+  end
 end
