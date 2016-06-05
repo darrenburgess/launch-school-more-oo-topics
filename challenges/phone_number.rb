6c8ecde143c0e56a7be3475f502b120ecd3395d9
Darren Burgess <darrentburgess@gmail.com>
2016-06-05 12:41:45 -0400

    small changes

--- challenges/phone_number.rb
+++ challenges/phone_number.rb
@@ -10,16 +10,31 @@ class PhoneNumber
     @phone_with_letters = phone_number.tr('^A-z0-9', '')
   end
 
+  def number
+    initial_cleanup
+    return INVALID if phone_with_letters.length > length
+    phone_number.slice!(0) if first_char_one? && length == 11
+    return INVALID if length < 10 || length == 11
+    return INVALID if length == 12 && first_char_one?
+    phone_number
+  end
+
+  def to_s
+    "(#{area_code}) #{local_exchange}-#{line_number}"
+  end
+
+  def area_code
+    number.slice(0, 3)
+  end
+
+  private
+
   def length
     phone_number.length
   end
 
   def first_char_one?
     phone_number.chr == "1"
-  end
-
-  def area_code
-    number.slice(0, 3)
   end
 
   def local_exchange
@@ -30,20 +45,8 @@ class PhoneNumber
     phone_number.slice(6, 4)
   end
 
-  def to_s
-    "(#{area_code}) #{local_exchange}-#{line_number}"
-  end
 
   def initial_cleanup
     phone_number.tr!('^0-9', '')
-  end
-
-  def number
-    initial_cleanup
-    return INVALID if phone_with_letters.length > length
-    phone_number.slice!(0) if first_char_one? && length == 11
-    return INVALID if length < 10 || length == 11
-    return INVALID if length == 12 && first_char_one?
-    phone_number
   end
 end
