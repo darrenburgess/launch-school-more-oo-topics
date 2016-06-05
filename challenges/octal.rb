9747992a6e18b9a246e4585be30ebf47b467963b
Darren Burgess <darrentburgess@gmail.com>
2016-06-05 19:06:13 -0400

    add basic stub of alternative octal converter

--- challenges/octal.rb
+++ challenges/octal.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-class Octal
+class Octal2
   attr_reader :number
   
   def initialize(number)
@@ -18,3 +18,41 @@ class Octal
     end.reduce(0, :+)
   end
 end
+
+class Octal
+  attr_reader :number
+  attr_accessor :array
+
+  def initialize(number)
+    @number = number
+  end
+
+  def invalid_octal?
+    number.match(/[\D89]/)
+  end
+
+  def to_decimal
+    return 0 if invalid_octal?
+    split_digits_into_array
+    reverse_array_of_digits
+    convert_each_digit
+    sum_values_in_array
+  end
+
+  private
+
+  def split_digits
+    array = number.to_s.chars
+  end
+
+  def reverse_array_of_digits
+    array.reverse!
+  end
+
+  def convert_each_digit
+  end
+
+  def sum_values_in_array
+  end
+end
+ 
