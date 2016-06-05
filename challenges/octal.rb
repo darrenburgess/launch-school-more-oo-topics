8ea1fab8f54e871d94613b87c78cd377b197b4d6
Darren Burgess <darrentburgess@gmail.com>
2016-06-05 18:00:31 -0400

    complete octal conversion

--- challenges/octal.rb
+++ challenges/octal.rb
@@ -7,6 +7,14 @@ class Octal
     @number = number
   end
 
+  def invalid_octal?
+    number.match(/[\D89]/)
+  end
+
   def to_decimal
+    return 0 if invalid_octal?
+    number.to_s.reverse.chars.each_with_index.map do |num, idx|
+      (num.to_i * 8 ** idx).to_i
+    end.reduce(0, :+)
   end
 end
