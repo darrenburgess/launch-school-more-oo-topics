2b5afb8b8cb00993495ec34f43d33a6e97e4f0e7
Darren Burgess <darrentburgess@gmail.com>
2016-06-05 12:16:49 -0400

    add phone number parsing - solved

---
+++ challenges/phone_number.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class PhoneNumber
+  attr_accessor :phone_number, :phone_with_letters
+
+  INVALID = "0000000000"
+
+  def initialize(phone_number)
+    @phone_number = phone_number
+    @phone_with_letters = phone_number.tr('^0-9A-Za-z', '')
+  end
+
+  def length
+    phone_number.length
+  end
+
+  def first_char_one?
+    phone_number.chr == "1"
+  end
+
+  def area_code
+    number.slice(0, 3)
+  end
+
+  def local_exchange
+    phone_number.slice(3, 3)
+  end
+
+  def line_number
+    phone_number.slice(6, 4)
+  end
+
+  def to_s
+    "(#{area_code}) #{local_exchange}-#{line_number}"
+  end
+
+  def initial_cleanup
+    phone_number.tr!('^0-9', '')
+  end
+
+  def number
+    initial_cleanup
+    return INVALID if phone_with_letters.length > length
+    phone_number.slice!(0) if first_char_one? && length == 11
+    return INVALID if length < 10 || length == 11
+    return INVALID if length == 12 && first_char_one?
+    phone_number
+  end
+end
