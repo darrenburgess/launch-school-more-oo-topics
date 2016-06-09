e4fef53101ccc7079f698025a736210530fb568f
Darren Burgess <darrentburgess@gmail.com>
2016-06-08 20:05:31 -0400

    resolve most rubocop offences

--- challenges/secret_handshake.rb
+++ challenges/secret_handshake.rb
@@ -4,7 +4,7 @@ class SecretHandshake
   attr_reader :number
   attr_accessor :result
 
-  def initialize number
+  def initialize(number)
     @number = number
     @result = []
   end
@@ -13,18 +13,18 @@ class SecretHandshake
     number.to_s(2)
   end
 
-  def is_true?(value)
+  def true?(value)
     value == '1'
   end
 
   def commands
     return [] if number.class == String
-    array = self.to_binary.split('').reverse
-    result << "wink" if is_true? array[0]
-    result << "double blink" if is_true? array[1]
-    result << "close your eyes" if is_true? array[2]
-    result << "jump" if is_true? array[3]
-    result.reverse! if is_true? array[4]
+    array = to_binary.split('').reverse
+    result << 'wink' if true? array[0]
+    result << 'double blink' if true? array[1]
+    result << 'close your eyes' if true? array[2]
+    result << 'jump' if true? array[3]
+    result.reverse! if true? array[4]
     result
   end
 end
