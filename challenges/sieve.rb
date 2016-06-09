02664c9fce6523b1139de00b681445d432fca527
Darren Burgess <darrentburgess@gmail.com>
2016-06-08 20:17:09 -0400

    resolve rubocop offenses

--- challenges/sieve.rb
+++ challenges/sieve.rb
@@ -8,7 +8,7 @@ class Sieve
     @numbers.each_with_index do |number, index|
       min_index = index + 1
       max_index = @numbers.size - 1
-      (min_index..max_index).each_with_index do |num, i|
+      (min_index..max_index).to_a.each_index do |i|
         @numbers.delete(number * (i + 2))
       end
       max_index = @numbers.size - 1
