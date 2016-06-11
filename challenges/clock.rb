27ad36d5d118ddb077351c24212ce6c121a7f1c8
Darren Burgess <darrentburgess@gmail.com>
2016-06-11 06:59:57 -0400

    refactor minus method to use time to minutes conversion

--- challenges/clock.rb
+++ challenges/clock.rb
@@ -10,16 +10,6 @@ class Clock
     new(hour, minute)
   end
 
-  def convert_hour
-    return "0#{@hour}" if @hour < 10
-    @hour
-  end
-
-  def convert_minute
-    return "0#{@minute}" if @minute < 10
-    @minute
-  end
-
   def +(minutes)
     @minute += minutes 
     @hour += @minute/60
@@ -28,21 +18,34 @@ class Clock
     self
   end
 
-  def -(minutes)
-    if minutes > @minute
-      @hour -= minutes / 60 + 1
-      @minute = @minute.to_i - minutes % 60 + 60
-    elsif minutes == @minute
-      @minute = 0
-    else
-      @hour -= minutes / 60 + 1 if minutes/60 >= 1
-      @minute = minutes % 60
-    end
+  def to_minutes
+    @hour * 60 + @minute
+  end
+
+  def to_time(minutes)
+    @hour = minutes / 60
+    @hour += 24 if @hour < 0
+    @minute = minutes % 60
     self
+  end
+
+  def -(minutes)
+    mins = to_minutes - minutes
+    to_time(mins)
   end
 
   def ==(other_clock)
     to_s == other_clock.to_s
+  end
+
+  def convert_hour
+    return "0#{@hour}" if @hour < 10
+    @hour
+  end
+
+  def convert_minute
+    return "0#{@minute}" if @minute < 10
+    @minute
   end
 
   def to_s
